e72967cbc16cd53f82fd06fe4f574c2c9220190b
Grigorii Horos <horosgrisa@gmail.com>
2021-06-17 15:42:08 +0300

    Add colors

--- index.js
+++ index.js
@@ -20,15 +20,17 @@ const colors = {
   cyan: { rotate: 340, saturate: 0.15, darken: 0.2 },
   teal: { rotate: 325, desaturate: 0.3, darken: 0.15 },
 
-  green: { rotate: 300, desaturate: 0.3, darken: 0.2 },
+  green: { rotate: 300, desaturate: 0.1, darken: 0.2 },
   yellow: { rotate: 210, saturate: 0.15, darken: 0.1 },
   orange: { rotate: 190, saturate: 0.5, darken: 0.15 },
 
   red: { rotate: 145, saturate: 0.15, darken: 0.2 },
   violet: { rotate: 80, desaturate: 0, darken: 0 },
-  magenta: { rotate: 115, saturate: 0.25, darken: 0.3 },
+  magenta: { rotate: 120, saturate: 0.25, darken: 0.3 },
 
-  brown: { rotate: 190, desaturate: 0.4, darken: 0.1 },
+  brown: { rotate: 210, desaturate: 0.7, darken: 0.15 },
+  "grey-cyan": { rotate: 330, desaturate: 0.7, darken: 0.15 },
+  "grey-violet": { rotate: 90, desaturate: 0.7, darken: 0.15 },
   grey: { desaturate: 1, darken: 0.1 },
 };
 
