3a0ba88bed5a5b05c936617f4d845cbbcf12f6f0
Grigorii Horos <horosgrisa@gmail.com>
2021-12-05 13:31:35 +0200

    New version

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ fi
 
 # rm -rf ./breeze-icons
 
-cp -Rpf --reflink=auto ./.icons/ ./breeze-icons
+#cp -Rpf --reflink=auto ./.icons/ ./breeze-icons
 
 sed -i -e 's/5\.82/5\.81/g' ./breeze-icons/CMakeLists.txt
 sed -i -e 's/5\.84/5\.82/g' ./breeze-icons/CMakeLists.txt
@@ -19,6 +19,8 @@ sed -i -e 's/5\.86/5\.84/g' ./breeze-icons/CMakeLists.txt
 sed -i -e 's/5\.87/5\.84/g' ./breeze-icons/CMakeLists.txt
 sed -i -e 's/5\.88/5\.84/g' ./breeze-icons/CMakeLists.txt
 sed -i -e 's/5\.89/5\.84/g' ./breeze-icons/CMakeLists.txt
+sed -i -e 's/5\.90/5\.84/g' ./breeze-icons/CMakeLists.txt
+sed -i -e 's/5\.91/5\.84/g' ./breeze-icons/CMakeLists.txt
 
 cd ./breeze-icons/icons
 python ../generate-24px-versions.py ./
