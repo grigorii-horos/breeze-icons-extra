abc1605c3561f3374addb0dd576f5bdbda7f35af
Grigorii Horos <horosgrisa@gmail.com>
2022-01-30 21:24:10 +0200

    Merge

--- build.sh
+++ build.sh
@@ -12,18 +12,14 @@ rm -rf ./breeze-icons
 
 cp -Rpf --reflink=auto ./.icons/ ./breeze-icons
 
-sed -i -e 's/5\.82/5\.81/g' ./breeze-icons/CMakeLists.txt
-sed -i -e 's/5\.84/5\.82/g' ./breeze-icons/CMakeLists.txt
-sed -i -e 's/5\.85/5\.82/g' ./breeze-icons/CMakeLists.txt
-sed -i -e 's/5\.86/5\.84/g' ./breeze-icons/CMakeLists.txt
-sed -i -e 's/5\.87/5\.84/g' ./breeze-icons/CMakeLists.txt
-sed -i -e 's/5\.88/5\.84/g' ./breeze-icons/CMakeLists.txt
-sed -i -e 's/5\.89/5\.84/g' ./breeze-icons/CMakeLists.txt
-sed -i -e 's/5\.90/5\.84/g' ./breeze-icons/CMakeLists.txt
-sed -i -e 's/5\.91/5\.84/g' ./breeze-icons/CMakeLists.txt
-sed -i -e 's/5\.92/5\.84/g' ./breeze-icons/CMakeLists.txt
-sed -i -e 's/5\.93/5\.84/g' ./breeze-icons/CMakeLists.txt
-sed -i -e 's/5\.94/5\.84/g' ./breeze-icons/CMakeLists.txt
+sed -i -e 's/5\.89/5\.88/g' ./breeze-icons/CMakeLists.txt
+sed -i -e 's/5\.90/5\.88/g' ./breeze-icons/CMakeLists.txt
+sed -i -e 's/5\.91/5\.88/g' ./breeze-icons/CMakeLists.txt
+sed -i -e 's/5\.92/5\.88/g' ./breeze-icons/CMakeLists.txt
+sed -i -e 's/5\.93/5\.88/g' ./breeze-icons/CMakeLists.txt
+sed -i -e 's/5\.94/5\.88/g' ./breeze-icons/CMakeLists.txt
+sed -i -e 's/5\.95/5\.88/g' ./breeze-icons/CMakeLists.txt
+sed -i -e 's/5\.96/5\.88/g' ./breeze-icons/CMakeLists.txt
 
 cd ./breeze-icons/icons
 python ../generate-24px-versions.py ./
@@ -38,6 +34,6 @@ node index.js
 
 cd ./breeze-icons
 
-find -name '*.svg' -type f | parallel -j "$(nproc)" ../node_modules/.bin/svgo --config ../config.cjs --input '{}' --output '{}' --quiet
+find -name '*.svg' -size +4k -type f | parallel -j "$(nproc)" ../node_modules/.bin/svgo --config ../config.cjs --input '{}' --output '{}' --quiet
 
 
