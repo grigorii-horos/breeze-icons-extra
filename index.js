a513c1347995c1a61664c868d9c5fb22fc2f0b51
Grigorii Horos <horosgrisa@gmail.com>
2022-10-14 23:48:52 +0300

    Update colors

--- index.js
+++ index.js
@@ -1,131 +1,140 @@
 /* eslint-disable no-await-in-loop, no-restricted-syntax */
-import mkdirp from 'mkdirp';
-import { existsSync } from 'node:fs';
-import {
-  readFile, symlink, unlink, writeFile,
-} from 'node:fs/promises';
-import { titleCase } from 'title-case';
+import mkdirp from "mkdirp";
+import { existsSync } from "node:fs";
+import { readFile, symlink, unlink, writeFile } from "node:fs/promises";
+import { titleCase } from "title-case";
 
 const iconsDirs = [
-  './breeze-icons/icons/places',
-  './breeze-icons/icons-dark/places',
+  "./breeze-icons/icons/places",
+  "./breeze-icons/icons-dark/places",
 ];
 
 const iconsOutDirs = [
-  './breeze-icons/icons/places',
-  './breeze-icons/icons-dark/places',
+  "./breeze-icons/icons/places",
+  "./breeze-icons/icons-dark/places",
 ];
 
-const smallIconSizes = ['16', '22', '24'];
-const largeIconSizes = ['32', '48', '64', '96'];
+const smallIconSizes = ["16", "22", "24"];
+const largeIconSizes = ["32", "48", "64", "96"];
 
 const iconSizes = [...smallIconSizes, ...largeIconSizes];
 
-const baseColors = ['#3daee9', '#232629', '#31363b'];
+const baseColors = ["#3daee9", "#232629", "#31363b"];
 
 const colors = {
-  default: ['#3daee9', '#232629'],
+  default: ["#3daee9", "#232629"],
 
-  red: ['#D32F2F', 'default'], //+
-  orange: ['#F57C00', 'default'], //+
-  yellow: ['#FBC02D', 'default'], //+
-  green: ['#388E3C', 'default'], //+
-  teal: ['#00796B', 'default'], //+
-  cyan: ['#0097A7', 'default'], //+
-  blue: ['#1976D2', 'default'], //+
-  violet: ['#512DA8', 'default'], //+
-  magenta: ['#7B1FA2', 'default'], //+
+  red: ["#eb0a42", "default"], //+
+  orange: ["#f89406", "default"], //+
+  yellow: ["#f2cb40", "default"], //+
+  green: ["#3bad7e", "default"], //+
+  teal: ["#00796B", "default"], //+
+  cyan: ["#21bbd7", "default"], //+
+  blue: ["#4183d7", "default"], //+
+  violet: ["#8e44ad", "default"], //+
+  magenta: ["#b5006a", "default"], //+
 
-  darkgrey: ['#5c5c5c', '#ffffff'], //-
-  grey: ['#a7afb4', '#000000'], //+
+  darkgrey: ["#5c5c5c", "#ffffff"], //-
+  grey: ["#a7afb4", "#000000"], //+
 };
 
-const actions = [
-  'activities',
-  'book',
-  'bookmark',
-  'cloud',
-  'comic',
-  'development',
-  'documents',
-  'download',
-  'favorites',
-  'games',
-  'gdrive',
-  'html',
-  'image-people',
-  'important',
-  'library',
-  'locked',
-  'mail',
-  'network',
-  'open',
-  'pictures',
-  'print',
-  'publicshare',
-  'root',
-  'script',
-  'sound',
-  'tar',
-  'temp',
-  'templates',
-  'text',
-  'unlocked',
-  'videos',
-  //
+const actions = {
+    // deb: { replaceColors: "#f62459" },
 
-  'appimage',
-  'build',
-  'calculate',
-  'chart',
-  'crash',
-  'database',
-  'design',
-  'desktop',
-  'docker',
-  'drawing',
-  'extension',
-  'flatpack',
-  'git',
-  'language',
-  'log',
-  'mac',
-  'notes',
-  'paint',
-  'presentation',
-  'sign',
-  'snap',
-  'table',
-  'windows',
-];
+  activities: {},
+  android: { replaceColors: "#9bd916" },
+  appimage: {},
+  blender: { replaceColors: "#f89406" },
+  book: {},
+  bookmark: {},
+  build: {},
+  calculate: {},
+  chart: {},
+  cloud: {},
+  comic: {},
+  crash: { replaceColors: "#eb0a42" },
+  database: {},
+  decrypted: { replaceColors: "#5c5c5c" },
+  design: {},
+  desktop: {},
+  development: {},
+  docker: {},
+  documents: {},
+  download: {},
+  drawing: {},
+  encrypted: { replaceColors: "#5c5c5c" },
+  extension: {},
+  favorites: {},
+  flatpak: {},
+  games: {},
+  gdrive: {},
+  git: {},
+  godot: {},
+  html: {},
+  important: { replaceColors: "#eb0a42" },
+  java: { replaceColors: "#ff8b23" },
+  language: {},
+  library: {},
+  locked: {},
+  log: {},
+  mac: {},
+  mail: {},
+  network: {},
+  notes: {},
+  open: {},
+  paint: {},
+  pictures: {},
+  podcast: { replaceColors: "#f89406" },
+  presentation: {},
+  print: {},
+  publicshare: {},
+  root: {},
+  rpm: { replaceColors: "#cf000f" },
+  script: {},
+  sign: {},
+  snap: {},
+  sound: {},
+  table: {},
+  tar: {},
+  temp: {},
+  templates: {},
+  text: {},
+  trash: { replaceColors: "#3bad7e" },
+  unlocked: {},
+  videos: {},
+  windows: {},
+
+    "image-people": {},
+
+};
 
 const links = {
-  downloads: 'download',
-  image: 'pictures',
-  images: 'pictures',
-  picture: 'pictures',
-  photo: 'pictures',
-  music: 'sound',
-  public: 'publicshare',
-  remote: 'html',
-  txt: 'text',
-  video: 'videos',
-  recent: 'temp',
-  home: 'user-home',
-  network: 'network-workgroup',
+  downloads: "download",
+  image: "pictures",
+  images: "pictures",
+  picture: "pictures",
+  photo: "pictures",
+  music: "sound",
+  public: "publicshare",
+  remote: "html",
+  txt: "text",
+  video: "videos",
+  recent: "temp",
+  home: "user-home",
+  network: "network-workgroup",
 };
 
 const linksCopies = {
-  'network-workgroup': 'network-workgroup',
-  'user-home': 'user-home',
+  "network-workgroup": "network-workgroup",
+  "user-home": "user-home",
 };
 
 const templates = [
   ...new Set([
-    ...actions,
+    ...Object.keys(actions),
     ...Object.keys(linksCopies),
     ...Object.values(links),
-    '',
+    "",
   ]),
 ];
 
@@ -149,7 +158,7 @@ const fn = async (iconsDir, iconsOutDir) => {
       try {
         await symlink(
           `${target}.svg`,
-          `${iconsOutDir}/${size}/folder-${path}.svg`,
+          `${iconsOutDir}/${size}/folder-${path}.svg`
         );
       } catch {
         // console.log(err)
@@ -161,30 +170,37 @@ const fn = async (iconsDir, iconsOutDir) => {
     for (const size of smallIconSizes) {
       for (const template of templates) {
         try {
-          if (color === 'default') {
+          if (color === "default") {
             let svg = await readFile(
-              `${iconsDir}/${size}/folder${template ? `-${template}` : ''}.svg`,
-              'UTF-8',
+              `${iconsDir}/${size}/folder${template ? `-${template}` : ""}.svg`,
+              "UTF-8"
             );
 
-            svg = svg.replaceAll(
-              '"fill:#da4453"',
-              '"fill:currentColor" class="ColorScheme-Text"',
-            );
+            if (actions[template].replaceColors) {
+              svg = svg.replaceAll(
+                '"fill:' + actions[template].replaceColors + '"',
+                '"fill:currentColor" class="ColorScheme-Text"'
+              );
+
+              // svg = svg.replaceAll(
+              //   '"fill:' + actions[template].replaceColors[0] + '"',
+              //   '"fill:currentColor" class="ColorScheme-Text"'
+              // );
+            }
 
             await writeFile(
               `${iconsOutDir}/${size}/folder${
-                template ? `-${template}` : ''
+                template ? `-${template}` : ""
               }.svg`,
-              svg,
+              svg
             );
 
             continue;
           }
 
           let svg = await readFile(
-            `${iconsDir}/${size}/folder${template ? `-${template}` : ''}.svg`,
-            'UTF-8',
+            `${iconsDir}/${size}/folder${template ? `-${template}` : ""}.svg`,
+            "UTF-8"
           );
 
           // const newColor = colors[color][0];
@@ -203,20 +219,29 @@ const fn = async (iconsDir, iconsOutDir) => {
           style += `    </style>
 `;
 
-          svg = svg.replaceAll('<style type="text/css" id="current-color-scheme">', `${style}\n    <style type="text/css" id="current-color-scheme">`);
-          svg = svg.replaceAll('<style id="current-color-scheme" type="text/css">', `${style}\n    <style type="text/css" id="current-color-scheme">`);
+          svg = svg.replaceAll(
+            '<style type="text/css" id="current-color-scheme">',
+            `${style}\n    <style type="text/css" id="current-color-scheme">`
+          );
+          svg = svg.replaceAll(
+            '<style id="current-color-scheme" type="text/css">',
+            `${style}\n    <style type="text/css" id="current-color-scheme">`
+          );
 
           svg = svg.replaceAll('class="ColorScheme-Text"', 'class="ColorText"');
-          svg = svg.replaceAll('class="ColorScheme-Highlight"', 'class="ColorHighlight"');
+          svg = svg.replaceAll(
+            'class="ColorScheme-Highlight"',
+            'class="ColorHighlight"'
+          );
 
           // svg = svg.replaceAll('ColorScheme-Text', 'ctn');
           // svg = svg.replaceAll('id="current-color-scheme"', '');
 
           await writeFile(
             `${iconsOutDir}/${size}/folder-${color}${
-              template ? `-${template}` : ''
+              template ? `-${template}` : ""
             }.svg`,
-            svg,
+            svg
           );
         } catch {
           // console.log(error)
@@ -231,30 +256,32 @@ const fn = async (iconsDir, iconsOutDir) => {
 
       for (const template of templates) {
         try {
-          if (color === 'default') {
+          if (color === "default") {
             let svg = await readFile(
-              `${iconsDir}/${size}/folder${template ? `-${template}` : ''}.svg`,
-              'UTF-8',
+              `${iconsDir}/${size}/folder${template ? `-${template}` : ""}.svg`,
+              "UTF-8"
             );
 
-            svg = svg.replaceAll(
-              '"fill:#eb0a42"',
-              '"fill:currentColor" class="ColorScheme-Highlight"',
-            );
+            if (actions[template].replaceColors) {
+              svg = svg.replaceAll(
+                '"fill:' + actions[template].replaceColors + '"',
+                '"fill:currentColor" class="ColorScheme-Highlight"'
+              );
+            }
 
             await writeFile(
               `${iconsOutDir}/${size}/folder${
-                template ? `-${template}` : ''
+                template ? `-${template}` : ""
               }.svg`,
-              svg,
+              svg
             );
 
             continue;
           }
 
           let svg = await readFile(
-            `${iconsDir}/${size}/folder${template ? `-${template}` : ''}.svg`,
-            'UTF-8',
+            `${iconsDir}/${size}/folder${template ? `-${template}` : ""}.svg`,
+            "UTF-8"
           );
 
           // svg = svg.replaceAll(baseColors[0], colors[color][0]);
@@ -264,7 +291,7 @@ const fn = async (iconsDir, iconsOutDir) => {
           let style = `
     <style type="text/css" >`;
 
-          if (colors[color][1] !== 'default') {
+          if (colors[color][1] !== "default") {
             style += `
     .ColorText {
       color:${colors[color][1]};
@@ -278,20 +305,32 @@ const fn = async (iconsDir, iconsOutDir) => {
     </style>
 `;
 
-          svg = svg.replaceAll('<style type="text/css" id="current-color-scheme">', `${style}\n    <style type="text/css" id="current-color-scheme">`);
-          svg = svg.replaceAll('<style id="current-color-scheme" type="text/css">', `${style}\n    <style type="text/css" id="current-color-scheme">`);
+          svg = svg.replaceAll(
+            '<style type="text/css" id="current-color-scheme">',
+            `${style}\n    <style type="text/css" id="current-color-scheme">`
+          );
+          svg = svg.replaceAll(
+            '<style id="current-color-scheme" type="text/css">',
+            `${style}\n    <style type="text/css" id="current-color-scheme">`
+          );
 
-          if (colors[color][1] !== 'default') {
-            svg = svg.replaceAll('class="ColorScheme-Text"', 'class="ColorText"');
+          if (colors[color][1] !== "default") {
+            svg = svg.replaceAll(
+              'class="ColorScheme-Text"',
+              'class="ColorText"'
+            );
           }
 
-          svg = svg.replaceAll('class="ColorScheme-Highlight"', 'class="ColorHighlight"');
+          svg = svg.replaceAll(
+            'class="ColorScheme-Highlight"',
+            'class="ColorHighlight"'
+          );
 
           await writeFile(
             `${iconsOutDir}/${size}/folder-${color}${
-              template ? `-${template}` : ''
+              template ? `-${template}` : ""
             }.svg`,
-            svg,
+            svg
           );
         } catch {
           // console.log(err)
@@ -301,7 +340,7 @@ const fn = async (iconsDir, iconsOutDir) => {
 
     for (const size of iconSizes) {
       for (const [path, target] of Object.entries(links)) {
-        if (color === 'default') {
+        if (color === "default") {
           continue;
         }
 
@@ -314,7 +353,7 @@ const fn = async (iconsDir, iconsOutDir) => {
         try {
           await symlink(
             `folder-${color}-${target}.svg`,
-            `${iconsOutDir}/${size}/folder-${color}-${path}.svg`,
+            `${iconsOutDir}/${size}/folder-${color}-${path}.svg`
           );
         } catch {
           // console.log(err)
@@ -328,15 +367,15 @@ fn(iconsDirs[0], iconsOutDirs[0]);
 fn(iconsDirs[1], iconsOutDirs[1]);
 
 const generateDesktopFiles = async () => {
-  const colorNames = Object.keys(colors).filter((color) => color !== 'default');
-  const actionsNames = actions;
+  const colorNames = Object.keys(colors).filter((color) => color !== "default");
+  const actionsNames = Object.keys(actions);
 
   let desktopFileColors = `[Desktop Entry]
 Type=Service
 MimeType=inode/directory
 Actions=${colorNames
-    .map((color, index) => `action${`${index}`.padStart(2, '0')};`)
-    .join('')}_SEPARATOR_;noColor;
+    .map((color, index) => `action${`${index}`.padStart(2, "0")};`)
+    .join("")}_SEPARATOR_;noColor;
 X-KDE-Priority=TopLevel
 X-KDE-StartupNotify=false
 X-KDE-Submenu=Change color
@@ -353,13 +392,13 @@ Exec=change-folder-icon color "" %U
   desktopFileColors += colorNames
     .map(
       (color, index) => `
-[Desktop Action action${`${index}`.padStart(2, '0')}]
-Name=${titleCase(color.replaceAll('-', ' '), { pascalCase: true })}
+[Desktop Action action${`${index}`.padStart(2, "0")}]
+Name=${titleCase(color.replaceAll("-", " "), { pascalCase: true })}
 Icon=folder-${color}
 Exec=change-folder-icon color ${color} %U
-`,
+`
     )
-    .join('');
+    .join("");
 
   //
 
@@ -367,8 +406,8 @@ Exec=change-folder-icon color ${color} %U
 Type=Service
 MimeType=inode/directory
 Actions=${actionsNames
-    .map((action, index) => `action${`${index}`.padStart(2, '0')};`)
-    .join('')}_SEPARATOR_;noLabel;
+    .map((action, index) => `action${`${index}`.padStart(2, "0")};`)
+    .join("")}_SEPARATOR_;noLabel;
 X-KDE-Priority=TopLevel
 X-KDE-StartupNotify=false
 X-KDE-Submenu=Change label
@@ -385,18 +424,18 @@ Exec=change-folder-icon label "" %U
   desktopFileActions += actionsNames
     .map(
       (action, index) => `
-[Desktop Action action${`${index}`.padStart(2, '0')}]
-Name=${titleCase(action.replaceAll('-', ' '), { pascalCase: true })}
+[Desktop Action action${`${index}`.padStart(2, "0")}]
+Name=${titleCase(action.replaceAll("-", " "), { pascalCase: true })}
 Icon=folder-${action}
 Exec=change-folder-icon label ${action} %U
-`,
+`
     )
-    .join('');
+    .join("");
 
   //
 
-  await writeFile('change-color.desktop', desktopFileColors);
-  await writeFile('change-label.desktop', desktopFileActions);
+  await writeFile("change-color.desktop", desktopFileColors);
+  await writeFile("change-label.desktop", desktopFileActions);
 };
 
 generateDesktopFiles();
